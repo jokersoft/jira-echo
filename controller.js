2e66b66fafefd73909193327318e0621c7645c8b
Yaroslav Sklabinsky <jokersoft.net@gmail.com>
2022-02-01 18:05:34 +0100

    debug logs, AUTHORIZATION_ENABLED

--- controller.js
+++ controller.js
@@ -1,3 +1,4 @@
+const AUTHORIZATION_ENABLED = process.env.AUTHORIZATION_ENABLED;
 const DEFAULT_JIRA_PORT = 443;
 const DEFAULT_JIRA_DNS = 'jira-echo.requestcatcher.com';
 const DEFAULT_PROJECT_ID = 10546;
@@ -11,12 +12,18 @@ const https = require('https');
 const express = require('express');
 
 const authMiddleware = (req, res, next) => {
+    if (AUTHORIZATION_ENABLED === "0") {
+        console.debug('AUTHORIZATION DISABLED!');
+        next();
+    }
+
     const { authorization } = req.headers;
     let expectedAuthHeaderValue = prepareAuthorizationHeader();
     if (authorization && authorization.includes(expectedAuthHeaderValue)) {
         console.debug('jira-echo authorization passed');
         next();
     } else {
+        console.debug('Forbidden');
         res.sendStatus(403);
     }
 };
@@ -28,6 +35,7 @@ function prepareAuthorizationHeader() {
 function gateway() {
     const router = express.Router();
     router.post('/ticket-created', (request, response) => {
+        console.debug('createTicket attempt');
         createTicket(request, response);
     });
 
@@ -47,6 +55,8 @@ function createTicket(globalRequest, globalResponse) {
     let ticket = globalRequest.body.issue;
     let summary = ticket.fields.summary;
     let description = ticket.fields.description;
+
+    console.debug(ticket);
 
     // prepare request
     let ticketRequest = {
@@ -77,15 +87,21 @@ function createTicket(globalRequest, globalResponse) {
         }
     }
 
+    console.debug('options');
+    console.debug(options);
+
     const req = https.request(options, res => {
         let responseData = '';
         res.on('data', function (chunk) {responseData += chunk;});
         res.on('end', function () {
+            console.debug('createTicket success');
             return globalResponse.status(res.statusCode).send(responseData);
         });
     })
 
     req.on('error', error => {
+        console.error('createTicket error');
+        console.error(error.message);
         return globalResponse.status(500).send(error.message);
     })
 
