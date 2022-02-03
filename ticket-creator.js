d7e10e5a7c2351293079fefdb63f7881395af38d
Yaroslav Sklabinsky <jokersoft.net@gmail.com>
2022-02-03 07:09:32 +0100

    work in progress

--- ticket-creator.js
+++ ticket-creator.js
@@ -1,62 +1,22 @@
-const AUTHORIZATION_ENABLED = process.env.AUTHORIZATION_ENABLED;
-const DEFAULT_JIRA_PORT = 443;
-const DEFAULT_JIRA_DNS = 'jira-echo.requestcatcher.com';
 const DEFAULT_PROJECT_ID = process.env.DEFAULT_PROJECT_ID ?? 10545;
-const DEFAULT_ISSUE_TYPE_ID = 10004;
-const JIRA_DNS = process.env.JIRA_DNS ?? DEFAULT_JIRA_DNS;
-const JIRA_PORT = process.env.JIRA_PORT ?? DEFAULT_JIRA_PORT;
-const ISSUE_TYPE_ID = process.env.ISSUE_TYPE_ID ?? DEFAULT_ISSUE_TYPE_ID;
+const PROJECT_SHERPA_ID = 10522;
+
+const INVOICING_PAYMENT_AND_TAXATION = 49;
+const WORKFLOW_AND_PROCESSES = 52;
+const EXTERNAL_TOOLS = 54;
 
 const https = require('https');
 const express = require('express');
 
-const authMiddleware = (req, res, next) => {
-    console.log('authMiddleware hit');
-    const { authorization } = req.headers;
-    let expectedAuthHeaderValue = prepareAuthorizationHeader();
-
-    if (AUTHORIZATION_ENABLED == 0) {
-        console.debug('AUTHORIZATION DISABLED!');
-        next();
-    } else if (req.query.authorization && decodeURIComponent(req.query.authorization).includes(expectedAuthHeaderValue)) {
-        console.debug('jira-echo query authorization passed');
-        next();
-    } else if (authorization && authorization.includes(expectedAuthHeaderValue)) {
-        console.debug('jira-echo header authorization passed');
-        next();
-    } else {
-        console.log('forbidden');
-        console.log('req.query.authorization');
-        console.log(req.query.authorization);
-        console.log('decodeURIComponent');
-        console.log(decodeURIComponent(req.query.authorization));
-        console.log('expectedAuthHeaderValue');
-        console.log(expectedAuthHeaderValue);
-        res.sendStatus(403);
+function getTargetProjectId(requestTypeId) {
+    if (requestTypeId === INVOICING_PAYMENT_AND_TAXATION ||
+        requestTypeId === WORKFLOW_AND_PROCESSES ||
+        requestTypeId === EXTERNAL_TOOLS
+    ) {
+        return PROJECT_SHERPA_ID;
     }
-};
 
-function prepareAuthorizationHeader() {
-    return 'Basic ' + Buffer.from(process.env.JIRA_USER + ':' + process.env.JIRA_TOKEN).toString('base64');
-}
-
-function gateway() {
-    const router = express.Router();
-    router.post('/ticket-created', (request, response) => {
-        console.debug('createTicket attempt');
-        createTicket(request, response);
-        //TODO: alert ticket creation
-    });
-
-    router.get('/webhooks', (request, response) => {
-        listWebhooks(request, response);
-    });
-
-    router.get('/health', (request, response) => {
-        health(request, response);
-    });
-
-    return router;
+    return DEFAULT_PROJECT_ID;
 }
 
 function createTicket(globalRequest, globalResponse) {
@@ -152,37 +112,4 @@ function createTicket(globalRequest, globalResponse) {
     req.end()
 }
 
-function listWebhooks(globalRequest, globalResponse) {
-    const options = {
-        hostname: JIRA_DNS,
-        port: JIRA_PORT,
-        path: '/rest/webhooks/1.0/webhook/',
-        method: 'GET',
-        headers: {
-            'Authorization': prepareAuthorizationHeader(),
-            'Accept': 'application/json',
-        }
-    }
-    const req = https.request(options, res => {
-        let responseData = '';
-        res.on('data', function (chunk) {responseData += chunk;});
-        res.on('end', function () {
-            //TODO: format webhooks nice
-            return globalResponse.status(res.statusCode).send(responseData);
-        });
-    })
-
-    req.on('error', error => {
-        return globalResponse.status(500).send(error.message);
-    })
-
-    req.end()
-}
-
-function health(request, response) {
-    response.status(200).send('{"status":"OK"}');
-}
-
-module.exports.health = (request, response) => health(request, response);
-module.exports.gateway = gateway;
-module.exports.authMiddleware = authMiddleware;
+module.exports.createTicket = (request, response) => createTicket(request, response);
