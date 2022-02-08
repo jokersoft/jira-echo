7d4f1e3c21f5d0ebbfb3405f40f3073a490948de
Yaroslav Sklabinsky <jokersoft.net@gmail.com>
2022-02-08 13:41:46 +0100

    tf example update

--- infrastructure/terraform/jira_echo.tf
+++ infrastructure/terraform/jira_echo.tf
@@ -9,6 +9,7 @@ locals {
     name = "jira-echo"
     fqdn = "jira-echo.${module.infra_vpc.public_subdomain}"
     url  = "https://jira-echo.${module.infra_vpc.public_subdomain}"
+    version = "v0.0.1"
   }
 }
 
@@ -20,11 +21,17 @@ resource "helm_release" "jira_echo" {
 
   values = [
     yamlencode({
+      imagePullSecrets = [
+        {
+          name = kubernetes_secret.infra_docker_hub_credentials.metadata[0].name
+        },
+      ]
+
       replicaCount = 1
 
       image = {
-        repository = "nginx"
-        tag        = "latest"
+        repository = "yarche/jira-echo"
+        tag        = local.jira_echo.version
       }
 
       ingress = {
@@ -47,6 +54,41 @@ resource "helm_release" "jira_echo" {
           }
         ]
       }
+      env = [
+        {
+          name  = "JIRA_ECHO_VERSION"
+          value = local.jira_echo.version
+        },
+        {
+          name  = "JIRA_DNS"
+          value = "comtravo.atlassian.net"
+        },
+        {
+          # default target project, 10545 = "BUGS" project
+          name  = "PROJECT_ID_DEFAULT"
+          value = "10545"
+        },
+        {
+          name  = "AUTHORIZATION_ENABLED"
+          value = "1"
+        },
+        {
+          name  = "JIRA_USER"
+          value = "yaroslav.sklabinskyi@comtravo.com"
+        },
+        {
+          name  = "JIRA_TOKEN"
+          value = data.aws_ssm_parameter.jira_echo_secret_token.value
+        },
+        {
+          name  = "SLACK_CHANNEL"
+          value = "C0320ML1G65"
+        },
+        {
+          name  = "SLACK_TOKEN"
+          value = data.aws_ssm_parameter.jira_echo_slack_token.value
+        },
+      ]
     })
   ]
 }
